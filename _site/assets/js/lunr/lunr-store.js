8144d7cacf16b3de11e72c3a65637deb9b6166c2
acrouviere <>
2021-01-07 22:32:05 +0100

    mise en route du site par ac

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,63 +5,21 @@ var store = [{
         "url": "http://localhost:4000/in%20the%20news/publication/brookings_retirement_tontines/",
         "teaser": null
       },{
-        "title": "Research Library",
-        "excerpt":"Nuovalo and Affiliated Research Partners State-sponsored Pensions for Private Sector Workers: The Case for Pooled Annuities and Tontines. Fullmer, R. K. and Forman, J. B. (2020). Wharton Pension Research Council Working Papers. Individual Tontine Accounts. Fullmer, R. K. and Sabin, M. J. (2019). Journal of Accounting and Finance, 19(8). Tontines:...","categories": ["Research","Publication"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/research/publication/research_library/",
-        "teaser": null
+        "title": "Rochet-Shchneider 26 six - Un post pour voir",
+        "excerpt":"Texte sldfjql lsdjkfsd mlkskjdflmqskjdf lmqsjdflqs                                                    Légende à compléter                                                        Légende à compléter                                                        Légende à compléter       ","categories": ["Rochet-Schneider 26 six","Restoration"],
+        "tags": ["Chapron"],
+        "url": "http://localhost:4000/rochet-schneider%2026%20six/restoration/RS26six-postessai/",
+        "teaser": "http://localhost:4000/assets/images/posts/gallery-rochet-schneider-26-six-7.jpg"
       },{
-        "title": "Tontines 101",
-        "excerpt":"The term “tontine” may be new to many people. This is not surprising, given that the once-common term largely fell out of use around one hundred years ago. Let’s review what a tontine is and take a quick look at its history… and why it is making a comeback. What...","categories": ["Explainer"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/explainer/tontines_101/",
-        "teaser": null
+        "title": "Le beau travail de Mathieu Bois",
+        "excerpt":"Texte le beau travail de Mathieu Bois                                                    Légende à compléter                                                        Légende à compléter                                                        Delahaye 135 MS Vedette Chapron - Traces d’apprêts.       ","categories": ["Delahaye 135 MS Cabriolet \"Vedette\" - Chapron","Restoration"],
+        "tags": ["Chapron"],
+        "url": "http://localhost:4000/delahaye%20135%20ms%20cabriolet%20%22vedette%22%20-%20chapron/restoration/Delahaye-135MS-Vedette/",
+        "teaser": "http://localhost:4000/assets/images/posts/image2.jpeg"
       },{
-        "title": "On the Matter of Fair and Equitable Tontine Design",
-        "excerpt":"Tontines are a type of investment in which the account balances of shareholders who die are forfeited, divided up, and transferred to shareholders who are still living. Exactly how they are divided up and transferred is a critical element of tontine design. Everyone should care about this whether they are...","categories": ["Explainer","Research"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/explainer/research/fair_tontine_design/",
-        "teaser": null
-      },{
-        "title": "Using Tontines to Manage Longevity Risk the Natural Way",
-        "excerpt":"Benjamin Franklin once famously wrote “… in this world nothing can be said to be certain, except death and taxes.” ¹ True, death is indeed certain. However, the timing of death is quite uncertain. This uncertainty greatly complicates financial planning by imparting upon we mortals the challenge of managing longevity...","categories": ["Explainer"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/explainer/managing_longevity_risk_the_natural_way/",
-        "teaser": null
-      },{
-        "title": "Individual Tontine Accounts – Yes, Seriously!",
-        "excerpt":"A version of this article also appeared in the Retirement Income Journal. Tontines are a much-misunderstood investment arrangement that deserve a fresh look. Put aside everything you thought you knew about them. Forget the devious plots found in fiction novels, and do not confuse modern day tontines with the sketchy,...","categories": ["Explainer","In the News"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/explainer/in%20the%20news/individual_tontine_accounts/",
-        "teaser": null
-      },{
-        "title": "Tontines: Might Retirees Prefer Noninsured Annuities?",
-        "excerpt":"Tontines, a nearly-extinct financial device invented centuries ago, are receiving fresh attention as a tool for retirement income. We do mean fresh – modern tontines envisioned by economists today have evolved since the tontines of yestercentury. Brief history Tontines were originally used by nations to finance wars against other nations....","categories": ["Explainer"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/explainer/might_retirees_prefer_noninsured_annuities/",
-        "teaser": null
-      },{
-        "title": "The Case for State-Sponsored Tontine Pensions for Private Sector Workers",
-        "excerpt":"This article first appeared in the Georgetown University Center for Retirement Initiatives blog. The quest for retirement security faces significant challenges in virtually every country across the globe. Traditional defined benefit (DB) pensions are challenged by increasing life expectancies, a lower ratio of workers to retirees, and historically low interest...","categories": ["In the News","Explainer"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/in%20the%20news/explainer/state_sponsored_tontine_pensions/",
-        "teaser": null
-      },{
-        "title": "A Practitioner's Primer on Tontine Portfolios",
-        "excerpt":"We are pleased to announce the CFA Institute Research Foundation has published Tontines: A Practitioner’s Guide to Mortality-Pooled Investments, a first of its kind primer for investment practitioners on the subject of tontine portfolios. Many thanks to the Foundation for funding this research brief. Available at/on: CFA Institute Research Foundation...","categories": ["In the News","Research","Publication"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/in%20the%20news/research/publication/tontine_primer/",
-        "teaser": null
-      },{
-        "title": "State-sponsored Pensions for Private Sector Workers: A Sustainable, Low-Cost Approach",
-        "excerpt":"This article, co-authored with Jonathan Barry Forman, first appeared in the RetireSecure Blog of the Wharton Pension Research Council. Oregon, Maryland, and a number of other US states have recently created state-sponsored retirement saving plans to help millions of private-sector workers lacking pension coverage to save for retirement. While these...","categories": ["In the News","Publication"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/in%20the%20news/publication/state_sponsored_private_sector_pensions/",
-        "teaser": null
-      },{
-        "title": "European Pensions: Back to the Future",
-        "excerpt":"“Tontines were wildly popular in the 18th and 19th centuries but their status suffered at the hands of fraudulent life insurers. Natalie Tuck explores whether tontines have a place within a modern European pensions system…” Our own Tyron Fouche is quoted in this European Pensions article exploring tontines and their...","categories": ["In the News","Publication"],
-        "tags": ["tontines"],
-        "url": "http://localhost:4000/in%20the%20news/publication/european_pensions_back_to_the_future/",
-        "teaser": "http://localhost:4000/assets/images/thb-alfa-romeo-giulietta-sprint-speciale-1300-ss.jpg"
+        "title": "Delahaye 135 MS Vedette Chapron - Il y a trois ans déjà : la restauration débute",
+        "excerpt":"Un détail qui dénote un très grand soin de la part du carrossier Chapron : les poignées de portes sont incluses dans la baguette. Delahaye 135 MS Vedette Chapron - Poignée incluse dans la baguette de porte. Delahaye 135 MS Vedette Chapron -Avant démontage. Delahaye 135 MS Vedette Chapron -...","categories": ["Delahaye 135 MS Cabriolet \"Vedette\" - Chapron","Restoration"],
+        "tags": ["Chapron"],
+        "url": "http://localhost:4000/delahaye%20135%20ms%20cabriolet%20%22vedette%22%20-%20chapron/restoration/Delahaye-135MS-Vedette/",
+        "teaser": "http://localhost:4000/assets/images/posts/Gouache_Vedette-1946.jpg"
       }]
